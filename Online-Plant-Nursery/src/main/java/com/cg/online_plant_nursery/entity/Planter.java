93d3b12d7c9c6e225b95f161216be91fbbe1f13c
SANJANA <SANJANA@LAPTOP-J088J7P1>
2021-04-03 17:19:51 +0530

    Planter class updated by sanjana

--- Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/entity/Planter.java
+++ Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/entity/Planter.java
@@ -1,5 +1,61 @@
 package com.cg.online_plant_nursery.entity;
 
 public class Planter {
+	@Entity
+	@Table(name="planter")
+	public class Planter {
+		@Id
+		@GeneratedValue(strategy=GenerationType.IDENTITY)
+		private int Id;
+		@Column
+		private String Name;
+		@Column
+		private String Type;
+		@Column
+		private double Price;
+		@Column
+		private String Image;
+		public Planter(int id, String name, String type, double price, String image) {
+			super();
+			Id = id;
+			Name = name;
+			Type = type;
+			Price = price;
+			Image = image;
+		}
+		public int getId() {
+			return Id;
+		}
+		public void setId(int id) {
+			Id = id;
+		}
+		public String getName() {
+			return Name;
+		}
+		public void setName(String name) {
+			Name = name;
+		}
+		public String getType() {
+			return Type;
+		}
+		public void setType(String type) {
+			Type = type;
+		}
+		public double getPrice() {
+			return Price;
+		}
+		public void setPrice(double price) {
+			Price = price;
+		}
+		public String getImage() {
+			return Image;
+		}
+		public void setImage(String image) {
+			Image = image;
+		}
+		
+		
+		
+
 
 }
