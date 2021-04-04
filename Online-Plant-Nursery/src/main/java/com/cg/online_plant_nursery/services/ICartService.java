44eba2d73d3253041bfafa02c11fa6b95583d472
DELL <DELL@DESKTOP-SJODGO7>
2021-04-04 11:38:24 +0530

    iCartService updated - sam

--- Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/services/ICartService.java
+++ Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/services/ICartService.java
@@ -1,5 +1,27 @@
 package com.cg.online_plant_nursery.services;
 
-public interface ICartService {
+import java.util.List;
 
+import com.cg.online_plant_nursery.entity.Cart;
+
+public interface ICartService {
+	public void addPlantToCart(long customerID,long plantID, int quantity);
+	public void addSeedToCart(long customerID,long seedID, int quantity);
+	public void addPlanterToCart(long customerID,long planterID, int quantity);
+	public void addGardenDecorToCart(long customerID,long gardenDecorID, int quantity);
+	public void addFertilizerToCart(long customerID,long fertilizerID, int quantity);
+	
+	public void removePlantFromCart(long customerID,long plantID);
+	public void removeSeedFromCart(long customerID,long seedID);
+	public void removePlanterFromCart(long customerID,long planterID);
+	public void removeGardenDecorFromCart(long customerID,long gardenDecorID);
+	public void removeFertilizerFromCart(long customerID,long fertilizerID);
+	
+	public void updatePlantQuantity(long customerID,long plantID, int quantity);
+	public void updateSeedQuantity(long customerID,long seedID, int quantity);
+	public void updatePlanterQuantity(long customerID,long planterID, int quantity);
+	public void updateGardenDecorQuantity(long customerID,long gardenDecorID, int quantity);
+	public void updateFertilizerQuantity(long customerID,long fertilizerID, int quantity);
+	
+	public List<Cart> viewCart();
 }
