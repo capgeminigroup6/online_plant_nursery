8eb89bd593a88e0a0c51ee78ea5ff957f0544d3a
SANJANA <SANJANA@LAPTOP-J088J7P1>
2021-04-05 00:57:19 +0530

    plant,planter controller updated

--- Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/controller/PlantController.java
+++ Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/controller/PlantController.java
@@ -1,5 +1,40 @@
 package com.cg.online_plant_nursery.controller;
 
+
+@RestController
+@RequestMapping("/plant")
 public class PlantController {
+	@Autowired
+	IPlantService service;
+	
+	@PostMapping("/addplant")
+	 public ResponseEntity<String> addPlant(@RequestBody Plant plant){
+	  service.addPlant(plant);
+	  return new ResponseEntity<String>("Plant Added",HttpStatus.OK);
+	 }
+	
+	 @GetMapping
+	 public ResponseEntity<List<Plant>> getAllPlants(){
+	  List<Plant> plantList=service.getAllPlants();
+	  return new ResponseEntity<List<Plant>>(plantList,HttpStatus.OK);
+	 }
+	
+	 @GetMapping("/getbyid")
+	 public ResponseEntity<Plant> getPlantById(@RequestBody int Id){
+	 Plant plant=service.getPlantById(Id);
+	  return new ResponseEntity<Plant>(plant,HttpStatus.OK);
+	 }
+	 
+	 @DeleteMapping("/delete/{PlantId}")
+		public ResponseEntity<String> removePlant(@PathVariable int PlantId){
+			service.removePlant(PlantId);
+			return new ResponseEntity<String>("deleted...", HttpStatus.OK);
+		}
+	 @PutMapping("/updateplant/{id}")
+	 public ResponseEntity<String> updatePlant(@PathVariable int id,@RequestBody Plant plant){
+	  service.updatePlant(id, plant);
+	  return new ResponseEntity<String>("updated",HttpStatus.OK);
+	 }
+	 
 
 }
--- Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/controller/PlanterController.java
+++ Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/controller/PlanterController.java
@@ -1,5 +1,38 @@
 package com.cg.online_plant_nursery.controller;
 
+@RestController
+@RequestMapping("/planter")
 public class PlanterController {
+	@Autowired
+	IPlanterService service;
+	
+	@PostMapping("/addplanter")
+	 public ResponseEntity<String> addPlanter(@RequestBody Planter planter){
+	  service.addPlanter(planter);
+	  return new ResponseEntity<String>("Planter Added",HttpStatus.OK);
+	 }
+	
+	 @GetMapping
+	 public ResponseEntity<List<Planter>> getAllPlanters(){
+	  List<Planter> planterList=service.getAllPlanters();
+	  return new ResponseEntity<List<Planter>>(planterList,HttpStatus.OK);
+	 }
+	
+	 @GetMapping("/getbyid")
+	 public ResponseEntity<Planter> getPlanterById(@RequestBody int Id){
+	 Planter planter=service.getPlanterById(Id);
+	  return new ResponseEntity<Planter>(planter,HttpStatus.OK);
+	 }
+	 
+	 @DeleteMapping("/delete/{PlanterId}")
+		public ResponseEntity<String> removePlanter(@PathVariable int PlanterId){
+			service.removePlanter(PlanterId);
+			return new ResponseEntity<String>("deleted...", HttpStatus.OK);
+		}
+	 @PutMapping("/updateplanter/{id}")
+	 public ResponseEntity<String> updatePlanter(@PathVariable int id,@RequestBody Planter planter){
+	  service.updatePlanter(id, planter);
+	  return new ResponseEntity<String>("updated",HttpStatus.OK);
+	 }
 
 }
