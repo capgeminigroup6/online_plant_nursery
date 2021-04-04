5e0c045ac56e4e8f62ff5694db497bf6bfe3c857
DELL <DELL@DESKTOP-SJODGO7>
2021-04-04 13:13:18 +0530

    orders,cart srevice class - sam

--- Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/dao/CartDAO.java
+++ Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/dao/CartDAO.java
@@ -1,5 +1,11 @@
 package com.cg.online_plant_nursery.dao;
 
-public interface CartDAO {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.online_plant_nursery.entity.Cart;
+
+@Repository
+public interface CartDAO extends JpaRepository<Cart, Integer>{
 
 }
--- Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/dao/OrderDetailsDAO.java
+++ Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/dao/OrderDetailsDAO.java
@@ -1,5 +1,11 @@
 package com.cg.online_plant_nursery.dao;
 
-public interface OrderDetailsDAO {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.online_plant_nursery.entity.OrderDetails;
+
+@Repository
+public interface OrderDetailsDAO extends JpaRepository<OrderDetails, Integer>{
 
 }
