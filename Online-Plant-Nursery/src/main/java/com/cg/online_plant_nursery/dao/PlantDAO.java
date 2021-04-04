18feff801b77faa9db9c930589486731a54eba2d
SANJANA <SANJANA@LAPTOP-J088J7P1>
2021-04-04 18:01:20 +0530

    Dao updated

--- Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/dao/PlantDAO.java
+++ Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/dao/PlantDAO.java
@@ -1,5 +1,9 @@
 package com.cg.online_plant_nursery.dao;
 
-public interface PlantDAO {
+@Repository
+public interface PlantDAO extends JpaRepository<Plant,Integer>{
 
+	@Query(value = "from Plant plant where plant.id=?1")
+	 public Plant getPlantById(int id);
+	 
 }
--- Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/dao/PlanterDAO.java
+++ Online-Plant-Nursery/src/main/java/com/cg/online_plant_nursery/dao/PlanterDAO.java
@@ -1,5 +1,9 @@
 package com.cg.online_plant_nursery.dao;
 
-public interface PlanterDAO {
-
+@Repository
+public interface PlanterDAO extends JpaRepository<Planter,Integer>{
+	
+	 @Query(value = "from Planter planter where planter.id=?1")
+	 public Planter getPlanterById(int id);
+	 
 }
